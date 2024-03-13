d8377b4a2b375a10797f7d2cb7b0a607a2e740ec
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-03-13 20:35:39 +0100

    fibonacci - both recursive and iterate approach

--- server/app.js
+++ server/app.js
@@ -4,6 +4,31 @@ const port = 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
+// Function to calculate the nth Fibonacci number - slower recursive approach 2.28 s for 40
+// function fibonacci(n) {
+//   if (n <= 0) {
+//     return 0;
+//   } else if (n == 1) {
+//     return 1;
+//   } else {
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+//   }
+// }
+
+// Function to calculate the nth Fibonacci number - faster approach 0.22 s for 40
+function fibonacci(n) {
+  let a = 0,
+    b = 1,
+    c;
+  if (n <= 0) return 0;
+  for (let i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
 // root path
 app.get("/", (req, res) => {
   res.send("Welcome to the server!");
@@ -23,6 +48,19 @@ app.get("/api/easyFunction", (req, res) => {
 
   const sum = num1 + num2;
   res.json({ sum });
+});
+
+//fibonacci endpoint POST
+app.post("/api/fibo", (req, res) => {
+  const { n } = req.body;
+
+  // Validate input(n must be a non-negative integer)
+  if (typeof n !== "number" || n < 0) {
+    return res.status(400).json({ error: "n must be a non-negative integer." });
+  }
+
+  const result = fibonacci(n);
+  res.json({ fibonacciNumber: result });
 });
 
 // Start the server on port 3000 and listen for incoming requests
